fc2cb0e7e1965274bc7b94af56db07378928f537
pakornkub <pakorn.worakarn@gmail.com>
2023-07-06 00:21:26 +0700

    update commit
    refactor
    - adjust function getDataFromQR read new format QR

--- src/utils/qr.ts
+++ src/utils/qr.ts
@@ -1,23 +1,24 @@
 export const getDataFromQR = (qr: string): any => {
+  try {
 
-    try {
+    //TODO: init Buffer for decode base64
+    const Buffer = require('buffer').Buffer;
 
-        const qrObject = JSON.parse(qr);
+    //TODO: get data qr from url "http://119.59.105.14/toto-warranty/service?info={data base64}"
+    let info = Buffer.from(qr?.split('?info=')[1], 'base64')?.toString() || '';
 
-        return qrObject[0];
-    
-    } catch (error:any) {
+    const qrObject = JSON.parse(info);
 
-        const err = {
-            status: false,
-            code: 'jsonError',
-            message: 'Could not parse json',
-            rawError: error
-        }
+    return qrObject[0];
+  } catch (error: any) {
+    console.log(error);
+    const err = {
+      status: false,
+      code: 'jsonError',
+      message: 'Could not parse json',
+      rawError: error,
+    };
 
-        return err;
-
-    }
-    
-  
+    return err;
+  }
 };
